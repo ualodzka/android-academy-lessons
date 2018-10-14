7e23d69b738dfe0acf5fdf9847b370d9b62ae83f
Vladimir LEzhnevich <v.lezhnevich@gmail.com>
2018-10-14 15:33:50 +0300

    Main and Preview screens implemented

--- app/src/main/java/com/example/vova/androidacademylessons/MainActivity.kt
+++ app/src/main/java/com/example/vova/androidacademylessons/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.vova.androidacademylessons
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        previewButton.setOnClickListener {
+            PreviewActivity.start(this, typeYourMessageEditText.text.toString())
+        }
     }
 }
