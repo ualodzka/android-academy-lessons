6f0d73fb3a1fc109e25d7ac715baee4b4d471bc3
Vladimir Lezhnevich <v.lezhnevich@gmail.com>
2018-10-19 13:59:12 +0300

    Links to social networks implemented

--- app/src/main/java/com/example/vova/androidacademylessons/MainActivity.kt
+++ app/src/main/java/com/example/vova/androidacademylessons/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.vova.androidacademylessons
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,19 +15,25 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
-   private fun setListeners() {
-       instagramImageView.setOnClickListener {
-           //launch instagram in browser
-       }
-       facebookImageView.setOnClickListener {
-           //launch facebook
-       }
-       sendMessageButton.setOnClickListener {
-//           val message = sendMessageEditText.text.toString()
+    private fun setListeners() {
+        instagramImageView.setOnClickListener {
+            startWebIntent("https://www.instagram.com/spongebob/")
+        }
+        facebookImageView.setOnClickListener {
+            startWebIntent("https://www.facebook.com/spongebob/")
+
+        }
+        sendMessageButton.setOnClickListener {
+            //           val message = sendMessageEditText.text.toString()
 //           val intent = Intent()
-       }
+        }
     }
 
+    private fun startWebIntent(url: String?) {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
 
 
 }
