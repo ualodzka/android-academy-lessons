3835a9292c0f984a82c0f3261e02d8ffaecc4125
Vladimir Lezhnevich <v.lezhnevich@gmail.com>
2018-10-19 13:41:10 +0300

    Main screen layouts implemented

--- app/src/main/java/com/example/vova/androidacademylessons/MainActivity.kt
+++ app/src/main/java/com/example/vova/androidacademylessons/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.vova.androidacademylessons
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
     }
+
+   private fun setListeners() {
+       instagramImageView.setOnClickListener {
+           //launch instagram in browser
+       }
+       facebookImageView.setOnClickListener {
+           //launch facebook
+       }
+       sendMessageButton.setOnClickListener {
+//           val message = sendMessageEditText.text.toString()
+//           val intent = Intent()
+       }
+    }
+
+
+
 }
+
+
